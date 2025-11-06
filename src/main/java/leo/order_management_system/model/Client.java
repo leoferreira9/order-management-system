3713e444ef8060fcdfae78b9b37b24ad1d11df10
Leonardo <leobarros_@outlook.com>
2025-11-05 22:33:52 -0300

    feat: add Client entity

---
+++ src/main/java/leo/order_management_system/model/Client.java
@@ -0,0 +1,50 @@
+package leo.order_management_system.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false, length = 150, unique = true)
+    private String email;
+
+    public Client(){}
+
+    public Client(Long id, String name, String email){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
