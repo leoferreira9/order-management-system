7655c85b145f782805066435e8671b8ba360ea6f
Leonardo <leobarros_@outlook.com>
2025-11-05 22:46:52 -0300

    feat: add Product entity and validation dependency

---
+++ src/main/java/leo/order_management_system/model/Product.java
@@ -0,0 +1,52 @@
+package leo.order_management_system.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false)
+    @DecimalMin(value = "0.00")
+    private Double price;
+
+    public Product() {}
+
+    public Product(Long id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
