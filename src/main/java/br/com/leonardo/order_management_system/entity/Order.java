d99c0c8c7198ba52250415660ee090f14fce256b
Leonardo <leobarros_@outlook.com>
2026-01-11 01:27:14 -0300

    feat: add order and order item relationship

---
+++ src/main/java/br/com/leonardo/order_management_system/entity/Order.java
@@ -0,0 +1,18 @@
+package br.com.leonardo.order_management_system.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    List<OrderItem> orderItemList = new ArrayList<>();
+}
