d663fe60b59e6a732dd0a6872c491b4ed5da0d48
Leonardo <leobarros_@outlook.com>
2026-01-15 16:05:35 -0300

    chore: add getters and setters to AddressCreateDTO

--- src/main/java/br/com/leonardo/order_management_system/dto/address/AddressCreateDTO.java
+++ src/main/java/br/com/leonardo/order_management_system/dto/address/AddressCreateDTO.java
@@ -34,4 +34,59 @@ public class AddressCreateDTO {
     @Pattern(regexp = "\\d{5}-\\d{3}")
     private String postalCode;
 
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(String neighborhood) {
+        this.neighborhood = neighborhood;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 }
