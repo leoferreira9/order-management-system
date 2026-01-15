3f8cf0ecb02aff7a48bc024cb11d0cdb65153b65
Leonardo <leobarros_@outlook.com>
2026-01-15 15:16:32 -0300

    feat: add product mapper

---
+++ src/main/java/br/com/leonardo/order_management_system/mapper/product/ProductMapper.java
@@ -0,0 +1,14 @@
+package br.com.leonardo.order_management_system.mapper.product;
+
+import br.com.leonardo.order_management_system.dto.product.ProductCreateDTO;
+import br.com.leonardo.order_management_system.dto.product.ProductDTO;
+import br.com.leonardo.order_management_system.entity.Product;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    ProductDTO toDto (Product product);
+
+    Product toEntity (ProductCreateDTO dto);
+}
