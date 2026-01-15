98192ad1bddf47c5ed52de9797710f4a53efe95b
Leonardo <leobarros_@outlook.com>
2026-01-15 17:07:35 -0300

    feat: add order item mapper

---
+++ src/main/java/br/com/leonardo/order_management_system/mapper/orderitem/OrderItemMapper.java
@@ -0,0 +1,14 @@
+package br.com.leonardo.order_management_system.mapper.orderitem;
+
+import br.com.leonardo.order_management_system.dto.orderitem.OrderItemCreateDTO;
+import br.com.leonardo.order_management_system.dto.orderitem.OrderItemDTO;
+import br.com.leonardo.order_management_system.entity.OrderItem;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface OrderItemMapper {
+
+    OrderItemDTO toDto (OrderItem orderItem);
+
+    OrderItem toEntity (OrderItemCreateDTO dto);
+}
